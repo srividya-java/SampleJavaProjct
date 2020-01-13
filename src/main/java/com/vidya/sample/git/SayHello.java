d5a967461469a15cf9a8f00b8a48a83f84e99bcc
SANUK\C0252082 <srividya.peddu@gmail.com>
2020-01-13 15:26:35 +0000

    modified text

---
+++ src/main/java/com/vidya/sample/git/SayHello.java
@@ -0,0 +1,8 @@
+package com.vidya.sample.git;
+
+public class SayHello {
+    public static void main(String args[])
+    {
+        System.out.println("Hi This is first commit to Git....");
+    }
+}
