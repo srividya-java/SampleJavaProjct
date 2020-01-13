8dd8cbf79a033f6675f2b5cba6c6ab047d0c7694
SANUK\C0252082 <srividya.peddu@gmail.com>
2020-01-13 14:49:38 +0000

    modified text

--- src/main/java/com/vidya/sample/git/SayHello.java
+++ src/main/java/com/vidya/sample/git/SayHello.java
@@ -3,6 +3,6 @@ package com.vidya.sample.git;
 public class SayHello {
     public static void main(String args[])
     {
-        System.out.println("Hi This is first commit to Git");
+        System.out.println("Hi This is first commit to Git....");
     }
 }
